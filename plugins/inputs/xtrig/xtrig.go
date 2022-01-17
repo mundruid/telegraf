77b512ca3e2262e4e88ecdb33cf79ddd48ca012e
Xenia Mountrouidou <drx@Xenias-MacBook-Pro.local>
2022-01-17 12:02:42 -0500

    add full plugin and configuration

--- plugins/inputs/xtrig/xtrig.go
+++ plugins/inputs/xtrig/xtrig.go
@@ -3,15 +3,18 @@ package xtrig
 import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
+	"math"
 )
 
 type Xtrig struct {
+	x         float64
+	Amplitude float64
 }
 
-var XtrigConfig = '
+var XtrigConfig = `
 	## Set the amplitude
 	amplitude = 10.0
-'
+`
 
 func (s *Xtrig) SampleConfig() string {
 	return XtrigConfig
@@ -22,9 +25,22 @@ func (s *Xtrig) Description() string {
 }
 
 func (s *Xtrig) Gather(acc telegraf.Accumulator) error {
+	sinner := math.Sin((s.x*math.Pi)/5.0) * s.Amplitude
+	cosinner := math.Cos((s.x*math.Pi)/5.0) * s.Amplitude
+
+	fields := make(map[string]interface{})
+	fields["sine"] = sinner
+	fields["cosine"] = cosinner
+
+	tags := make(map[string]string)
+
+	s.x += 1.0
+	// acc: instantiation of accumulator
+	acc.AddFields("trig", fields, tags)
+
 	return nil
 }
 
 func init() {
-	inputs.Add("xtrig", func() telegraf.Input { return &Xtrig{} })
+	inputs.Add("xtrig", func() telegraf.Input { return &Xtrig{x: 0.0} })
 }
